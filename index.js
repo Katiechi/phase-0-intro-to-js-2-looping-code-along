e3d55a150627a3f76b59833ec13e21cdd1200139
brian ambundo <brian.katiechi9@gmail.com>
2022-06-13 15:54:23 +0300

    commit tree b5bd084be3ede486697dddd7a6db95eb29132a3e

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solutions in this file
+'use strict'
+const majina=["Charlie", "Samip", "Ali"]
+const sherehe="birthday"
+
+
+function writeCards(stringNames,eventName){
+    let newEmptyArray=[]
+    for (let i=0; i<stringNames.length; i++){
+        
+        newEmptyArray.push((`Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`))
+        
+        
+    }
+    return newEmptyArray
+}
+console.log(writeCards(majina,sherehe))
+
+function countDown(n){
+  let hesabu = n+1
+  while (hesabu > 0){
+    hesabu--;
+    console.log(hesabu);
+}
+
+}
+countDown(10)
+
+
